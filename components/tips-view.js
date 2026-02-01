4e3bdcb9ee016e511f917310bc403c957f03f3c6
Hue <skeptikal.monke@gmail.com>
2026-02-01 23:22:08 +0100

    tips view component

---
+++ components/tips-view.js
@@ -0,0 +1,13 @@
+
+export const default TipsView = ({forId}) => {
+	const { forId } = props
+	const [tips, setTips] = useState(null)
+	const [loading, setLoading] = useState(true)
+	const [error, setError] = useState(null)
+
+	useEffect(() => {
+		// fetch tips from firebase
+	})
+
+
+}
