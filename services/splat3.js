eec1f88e7e475d0c48c0019232c75f630bbd4931
Hue <skeptikal.monke@gmail.com>
2026-02-14 22:56:53 +0100

    add subqueries

--- services/splat3.js
+++ services/splat3.js
@@ -3,3 +3,11 @@ import {get_w_cache} from "./api_cache.js"
 export const schedules_all = async () => {
 	return await get_w_cache("https://splatoon3.ink/data/schedules.json", 60 * 60 * 1000)
 }
+
+export const turf_sched = async () => {
+	return await schedules_all.regularSchedules.nodes
+}
+
+export const anarchy_sched = async () => {
+	return await schedules_all.bankaraSchedules.nodes
+}
