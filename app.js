88ac5f01296057f0e49f9e8a80b90e2a177611be
Hue <skeptikal.monke@gmail.com>
2026-02-15 00:55:03 +0100

    navigation

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import { Home } from "./screens/home.js"
 import { Modes } from "./screens/modes.js"
+import { ScheduleTurf } from "./screens/schedules-turf.js"
+import { ScheduleSeries } from "./screens/schedules-series.js"
+import { ScheduleOpen } from "./screens/schedules-open.js"
+import { ScheduleX } from "./screens/schedules-x.js"
 
 const Stack = createNativeStackNavigator()
 
@@ -11,6 +15,10 @@ export default function app(){
 			<Stack.Navigator>
 				<Stack.Screen name="home" component={Home} />
 				<Stack.Screen name="modes" component={Modes} />
+				<Stack.Screen name="schedules-turf" component={ScheduleTurf} />
+				<Stack.Screen name="schedules-series" component={ScheduleSeries} />
+				<Stack.Screen name="schedules-open" component={ScheduleOpen} />
+				<Stack.Screen name="schedules-x" component={ScheduleX} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	)
