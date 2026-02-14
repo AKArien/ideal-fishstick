280aafd4a19132265e9cdbba1ffc6201a780eafb
Hue <skeptikal.monke@gmail.com>
2026-02-14 22:56:03 +0100

    adjust schedules

--- components/schedules.js
+++ components/schedules.js
@@ -1,33 +1,50 @@
 
-export const generic_schedule = ({navigate, nodes, render}) => {
+export const Schedule = ({navigation, route}) => {
+	const {get_sched_fn} = route.params
 
-	const render_stage = ({stage}) => {
+	const [sched, setSched] = useState(null);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		async function get(){
+			try {
+				setSched(await get_sched_fn())
+				setLoading(false)
+			}
+			catch (error){
+				setLoading(false)
+				setError(error)
+			}
+		}
+		get()
+	}, [])
+
+	const RenderStage = ({stage}) => {
 		return (
-			<Pressable onPress={navigation.navigate(stage)}>
-				<Image
-					style = {styles.mapArt}
-					source = {stage.image.url}
-				/>
-				<Text>
-					{stage.name}
-				</Text>
-			</Pressable>
+			<Card
+				text={stage.name}
+				img={stage.image.url}
+				goto={"map-detail", stage.vsStageId}
+				navigation={navigation}
+			/>
 		)
 	}
 
-	const render_section = ({item}) => {
+	const render_section = (item) => {
 		return (
 			<>
-				
-				<render_stage stage={item.regularMatchSetting.vsStages."0"} />
-				<render_stage stage={item.regularMatchSetting.vsStages."1"} />
+				<Text>Current stages</Text>
+				<RenderStage stage={item."0"} />
+				<RenderStage stage={item."1"} />
 			</>
+		)
 	}
 
 	return (
 		<>
 			<FlatList
-				data = {nodes}
+				data = {vsStages}
 				renderItem={render_section}
 				keyExtractor={(item, index) => item[index]}
 			/>
