a38c8d33d3fcfc239692f845e36dc04613029305
Hue <skeptikal.monke@gmail.com>
2026-02-15 16:18:33 +0100

    add weapon field to map detail to filter

--- screens/map-detail.js
+++ screens/map-detail.js
@@ -1,22 +1,31 @@
+import { useState } from 'react'
+import { View } from 'react-native'
+
 import {Card} from "../components/cards"
 import {NewTip} from "../components/new-tip"
 import {TipsView} from "../components/tips-view"
+import {WeaponPicker} from "../components/weapon-picker"
 
 export const MapDetail = ({navigation, route}) => {
 	const { id, name, img } = route.params
+	const [filterWeapon, setFilterWeapon] = useState(null)
 
-	// todo : put throbber colour by the match mode
 	return (
 		<>
 			<Card
 				text={name}
 				img={img}
 			/>
+			<WeaponPicker
+				selectedWeapon={filterWeapon}
+				onWeaponSelect={setFilterWeapon}
+			/>
 			<NewTip
 				forId={id}
 			/>
 			<TipsView
 				forId={id}
+				filterWeapon={filterWeapon}
 			/>
 		</>
 
