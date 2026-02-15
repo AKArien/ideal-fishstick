cd8a508f37ae0fe86a05be637a0b909d5fb1ddf2
Hue <skeptikal.monke@gmail.com>
2026-02-15 10:55:49 +0100

    navigation is optional

--- components/cards.js
+++ components/cards.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, Pressable, Image } from 'react-native'
 export const Card = ({text, img, goto, gotoArg, navigation} ) => {
 
 	return (
-		<Pressable onPress={() => navigation.navigate(goto, gotoArg)}>
+		<Pressable onPress={() => {if (navigation){ navigation.navigate(goto, gotoArg)}}}>
 			<Image
 				style={styles.image}
 				source={{uri: img}}
