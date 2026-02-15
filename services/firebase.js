a4d2858d43aebffe5e7e272e4819a9e9229ac13a
Hue <skeptikal.monke@gmail.com>
2026-02-15 14:28:42 +0100

    included firebase.js, now relying on firebase-config export

---
+++ services/firebase.js
@@ -0,0 +1,76 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore,collection,addDoc,query,orderBy,where,onSnapshot,
+  doc,
+  updateDoc,
+  deleteDoc
+} from 'firebase/firestore'
+
+import {firebaseConfig} from "./firebase-config.js"
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+export const getTips = (callback) => {
+  const q = query(collection(db, 'tips'), orderBy('date', 'desc'))
+  onSnapshot(q, snapshot => {
+    let tips = []
+    snapshot.forEach(doc => {
+      tips.push({ id: doc.id, ...doc.data() })
+    })
+    callback(tips)
+  })
+}
+
+export const getTipsFiltered = (mapId, callback) => {
+  let q
+  
+  if (mapId !== null && mapId !== undefined) {
+    q = query(
+      collection(db, 'tips'),
+      where('map', '==', mapId),
+      orderBy('date', 'desc')
+    )
+  } else {
+    q = query(collection(db, 'tips'), orderBy('date', 'desc'))
+  }
+  
+  return onSnapshot(q, snapshot => {
+    let tips = []
+    snapshot.forEach(doc => {
+      tips.push({ id: doc.id, ...doc.data() })
+    })
+    callback(tips)
+  })
+}
+
+export const addTip = (tip) => {
+  const tipData = {
+    ...tip,
+    weapon: tip.weapon ? doc(db, 'weapons', tip.weapon) : null
+  }
+  addDoc(collection(db, 'tips'), tipData)
+}
+
+export const updateTip = tip => {
+	const updateData = {
+		content: tip.content,
+		weapon: tip.weapon ? doc(db, 'weapons', tip.weapon) : null
+	}
+	updateDoc(doc(db, 'tips', tip.id), updateData)
+}
+
+export const deleteTip = tip => {
+  deleteDoc(doc(db, 'tips', tip.id))
+}
+
+
+export const getWeapons = (callback) => {
+  const q = query(collection(db, 'weapons'), orderBy('class', 'name'))
+  onSnapshot(q, snapshot => {
+    let weapons = []
+    snapshot.forEach(doc => {
+      weapons.push({ id: doc.id, ...doc.data() })
+    })
+    callback(weapons)
+  })
+}
